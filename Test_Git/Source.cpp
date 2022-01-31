bb5e124bbb4e1a61f3dfd781e09feadf5ba6e10a
danik <=>
2022-01-31 16:27:42 +0200

    Point commit

--- Test_Git/Source.cpp
+++ Test_Git/Source.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 using namespace std;
 
+class Point {
+	int x;
+	int y;
+public:
+	Point(int x, int y) {
+		this->x = x;
+		this->y = y;
+	}
+	Point() {
+		this->x = 0;
+		this->y = 0;
+	}
+	int get_x() {
+		return x;
+	}
+	int get_y() {
+		return y;
+	}
+};
+
 int main() {
 
 	return 0;
