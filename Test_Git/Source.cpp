bdb5cd3ba65cae436a475816641ab7d541fa738d
danik <=>
2022-01-31 16:36:25 +0200

    Figure commit

--- Test_Git/Source.cpp
+++ Test_Git/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Point {
@@ -10,14 +11,40 @@ public:
 		this->y = y;
 	}
 	Point() {
-		this->x = 0;
-		this->y = 0;
+		x = 0;
+		y = 0;
 	}
 	int get_x() {
 		return x;
 	}
 	int get_y() {
 		return y;
+	}
+	void set_x() {
+		cin >> x;
+	}
+	void set_y() {
+		cin >> y;
+	}
+};
+
+class Figure {
+	vector<Point> polygons;
+public:
+	Figure(int size) {
+		int x{ 0 };
+		int y{ 0 };
+
+		for (int i = 0; i < size; i++) {
+			cin >> x;
+			cin >> y;
+			polygons.push_back(Point(x, y));
+		}
+	}
+	void print() {
+		for (auto it = polygons.begin(); it != polygons.end(); it++) {
+			cout << "X " << it->get_x() << " : " << "Y " << it->get_y() << "\n";
+		}
 	}
 };
 
